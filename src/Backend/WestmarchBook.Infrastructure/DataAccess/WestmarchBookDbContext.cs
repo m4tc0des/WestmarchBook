a3dd5678d42443b245fa832fa6407cdd561ba3a1
Mateus <matt_ufo2@hotmail.com>
2026-08-21 17:42:11 -0300

    feat(infrastructure): implementa WestmarchBookDbContext com suporte ao EF Core

---
+++ src/Backend/WestmarchBook.Infrastructure/DataAccess/WestmarchBookDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WestmarchBook.Domain.Entities;
+
+namespace WestmarchBook.Infrastructure.DataAccess;
+
+internal sealed class WestmarchBookDbContext : DbContext
+{
+    public WestmarchBookDbContext(DbContextOptions options) : base(options)
+    {
+
+    }
+
+    public DbSet<Users> Users { get; set; }
+}
