c747c255d17fd124fed669a67113b6545096ad15
Mateus <matt_ufo2@hotmail.com>
2026-08-22 11:42:56 -0300

    feat(infrastructure): adiciona contrato e implementação do UnitOfWork
    - Criada a interface IUnitOfWork na camada de domínio com o método Commit.
    - Implementada a classe UnitOfWork na infraestrutura fazendo o wrapping do SaveChangesAsync do EF Core.

---
+++ src/Backend/WestmarchBook.Infrastructure/DataAccess/UnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using WestmarchBook.Domain.Repositories;
+
+namespace WestmarchBook.Infrastructure.DataAccess;
+
+internal class UnitOfWork: IUnitOfWork
+{
+    private readonly WestmarchBookDbContext _dbContext;
+
+    public UnitOfWork(WestmarchBookDbContext dbContext)
+    {
+        _dbContext = dbContext;    
+    }
+
+    public async Task Commit()
+    {
+        await _dbContext.SaveChangesAsync();
+    }
+}
