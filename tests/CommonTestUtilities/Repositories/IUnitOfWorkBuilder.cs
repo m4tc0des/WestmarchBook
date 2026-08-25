7432b442e018ce453914ea744c49d31962414af0
Mateus <matt_ufo2@hotmail.com>
2026-08-25 18:00:38 -0300

    test(common): adiciona builders de testes para UnitOfWork e PasswordHasher
    - Criada a classe IUnitOfWorkBuilder para instanciar mocks do IUnitOfWork de forma padronizada.
    - Criada a classe IPasswordHasherBuilder utilizando o Moq para configurar o comportamento do hash e da verificação de senhas nos testes.

---
+++ tests/CommonTestUtilities/Repositories/IUnitOfWorkBuilder.cs
@@ -0,0 +1,14 @@
+﻿using Moq;
+using WestmarchBook.Domain.Repositories;
+
+namespace CommonTestUtilities.Repositories;
+
+public class IUnitOfWorkBuilder
+{
+    public static IUnitOfWork Build()
+    {
+        var mock = new Mock<IUnitOfWork>();
+
+        return mock.Object;
+    }
+}
---
+++ tests/CommonTestUtilities/Security/IPasswordHasherBuilder.cs
@@ -0,0 +1,27 @@
+﻿using Moq;
+using WestmarchBook.Domain.Repositories.User;
+using WestmarchBook.Domain.Security.PasswordHashing;
+
+namespace CommonTestUtilities.Security;
+
+public class IPasswordHasherBuilder
+{
+    private readonly Mock<IPasswordHasher> _mock;
+
+    public IPasswordHasherBuilder()
+    {
+        _mock = new Mock<IPasswordHasher>();
+
+        _mock.Setup(options => options.Ha(It.IsAny<string>()).Re;
+    }
+
+    public void ExistActiveUserWithEmail(string email)
+    {
+        _mock.Setup(options => options.ExisteActiveUserWithEmail(email)).ReturnsAsync(true);
+    }
+
+    public IPasswordHasher Build()
+    {
+        return _mock.Object;
+    }
+}
