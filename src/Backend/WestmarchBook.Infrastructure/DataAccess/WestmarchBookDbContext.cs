4b7dd03c9edc5df71a0fd88ac4add2169cdecd0a
Mateus <matt_ufo2@hotmail.com>
2026-08-26 17:45:53 -0300

    refactor(infrastructure): adiciona InternalsVisibleTo para testes e limpa construtor do DbContext
    - Adicionada a diretiva [assembly: InternalsVisibleTo("WebApi.Tests")] para permitir o acesso ao DbContext interno a partir do projeto de testes de integração.
    - Limpo e simplificado o construtor da classe WestmarchBookDbContext.

--- src/Backend/WestmarchBook.Infrastructure/DataAccess/WestmarchBookDbContext.cs
+++ src/Backend/WestmarchBook.Infrastructure/DataAccess/WestmarchBookDbContext.cs
@@ -1,14 +1,13 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.Runtime.CompilerServices;
 using WestmarchBook.Domain.Entities;
 
+[assembly:InternalsVisibleTo("WebApi.Tests")]
 namespace WestmarchBook.Infrastructure.DataAccess;
 
 internal sealed class WestmarchBookDbContext : DbContext
 {
-    public WestmarchBookDbContext(DbContextOptions options) : base(options)
-    {
+    public WestmarchBookDbContext(DbContextOptions options) : base(options) { }
 
-    }
-
-    public DbSet<Users> Users { get; set; }
+    public DbSet<User> Users { get; set; }
 }
---
+++ src/Shared/WestmarchBook.Exception/ExceptionsBase/WestmarchBookException.cs
@@ -0,0 +1,9 @@
+﻿using System.Net;
+
+namespace WestmarchBook.Exception.ExceptionsBase;
+
+public abstract class WestmarchBookException: System.Exception
+{
+    public abstract HttpStatusCode GetStatusCode();
+    public abstract List<string> GetErrorMessage();
+}
