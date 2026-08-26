92a45af6940947474110b7bc074aa86593c46fa4
Mateus <matt_ufo2@hotmail.com>
2026-08-26 10:43:05 -0300

    test(common): adiciona builders de testes para os repositórios de usuário
    - Criada a classe IUserReadOnlyRepositoryBuilder utilizando o Moq para configurar o comportamento de leitura do repositório.
    - Criada a classe IUserWriteOnlyRepositoryBuilder para instanciar mocks do IUserWriteOnlyRepository de forma padronizada.

---
+++ tests/CommonTestUtilities/Repositories/IUserReadOnlyRepositoryBuilder.cs
@@ -0,0 +1,24 @@
+﻿using Moq;
+using WestmarchBook.Domain.Repositories.User;
+
+namespace CommonTestUtilities.Repositories;
+
+public class IUserReadOnlyRepositoryBuilder
+{
+    private readonly Mock<IUserReadOnlyRepository> _mock;
+
+    public IUserReadOnlyRepositoryBuilder()
+    {
+        _mock = new Mock<IUserReadOnlyRepository>();
+    }
+
+    public void ExistActiveUserWithEmail(string email)
+    {
+        _mock.Setup(options => options.ExisteActiveUserWithEmail(email)).ReturnsAsync(true);
+    }
+
+    public IUserReadOnlyRepository Build()
+    {
+        return _mock.Object;
+    }
+}
--- tests/Validators.Tests/User/Register/RegisterUserAccountValidatorTests.cs
+++ tests/Validators.Tests/User/Register/RegisterUserAccountValidatorTests.cs
@@ -42,6 +42,8 @@ public class RegisterUserAccountValidatorTests
         });
     }
 
+
+
     [Theory]
     [InlineData("")]
     [InlineData(null)]
