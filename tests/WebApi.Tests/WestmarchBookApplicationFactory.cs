b1faf36210b9bad031b0a3fccdf4a2c7759fd6dd
Mateus <matt_ufo2@hotmail.com>
2026-08-26 10:48:21 -0300

    test(integration): adiciona WestmarchBookApplicationFactory com Testcontainers para testes de API
    - Criada a classe WestmarchBookApplicationFactory herdando de WebApplicationFactory<Program> e implementando IAsyncLifetime.
    - Configurada a inicialização e o encerramento de um container MySQL isolado via Testcontainers para substituir a string de conexão nos testes de integração.

---
+++ tests/WebApi.Tests/WestmarchBookApplicationFactory.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;
+using Testcontainers.MySql;
+
+namespace WebApi.Tests;
+
+public class WestmarchBookApplicationFactory : WebApplicationFactory<Program>, IAsyncLifetime
+{
+    private readonly MySqlContainer _mySqlContainer;
+
+    public WestmarchBookApplicationFactory()
+    {
+        _mySqlContainer = new MySqlBuilder("mysql:8.0").WithDatabase("westmarchbook").Build();
+    }
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.UseEnvironment("Tests")
+            .ConfigureAppConfiguration((_, options) =>
+            {
+                var parameters = new Dictionary<string, string?>
+                {
+                    ["ConnectionStrings:DbConnection"] = _mySqlContainer.GetConnectionString()
+                };
+
+                options.AddInMemoryCollection(parameters);
+            });
+    }
+
+    public async Task InitializeAsync()
+    {
+        await _mySqlContainer.StartAsync();
+    }
+
+    Task IAsyncLifetime.DisposeAsync()
+    {
+        return _mySqlContainer.StopAsync();
+    }
+}
