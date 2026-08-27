dc032f33d58d1fcc40d298f3d1b05f5d6dbbe333
Mateus <matt_ufo2@hotmail.com>
2026-08-27 10:37:38 -0300

    feat(api): adiciona AuthenticationController com endpoint de login
    - Criado o AuthenticationController com o endpoint POST para autenticação de usuários.
    - Injetado o caso de uso ILoginWithEmailAndPasswordUseCase para processar a credencial enviada no RequestLoginJson.
    - Mapeadas as respostas da API para os status 200 OK (com ResponseRegisterUserJson) e 401 Unauthorized (com ResponseErrorJson).

---
+++ src/Backend/WestmarchBook.Api/Controllers/AuthenticationController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WestmarchBook.Application.UseCases.Login.WithEmailAndPassword;
+using WestmarchBook.Communication.Requests;
+using WestmarchBook.Communication.Responses;
+
+namespace WestmarchBook.Api.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class AuthenticationController : ControllerBase
+{
+    [HttpPost]
+    [ProducesResponseType(typeof(ResponseRegisterUserJson), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ResponseErrorJson), StatusCodes.Status401Unauthorized)]
+    public async Task<IActionResult> Login([FromBody] RequestLoginJson request, [FromServices] ILoginWithEmailAndPasswordUseCase useCase)
+    {
+        var response = await useCase.Execute(request);
+
+        return Ok(response);
+    }
+}
