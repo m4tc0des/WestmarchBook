39e1046ca466dee8bbf32279953b5fd8ef7526fa
Mateus <matt_ufo2@hotmail.com>
2026-08-27 10:00:09 -0300

    test(common): adiciona classe EmptyNullOrBlankSpace para padronizar dados de teste de strings
    - Criada a classe EmptyNullOrBlankSpace em CommonTestUtilities.ErrorsClassData implementando IEnumerable<object[]> para fornecer cenários reutilizáveis de strings vazias, nulas e com espaços em branco para testes parametrizados.

---
+++ tests/CommonTestUtilities/ErrorsClassData/EmptyNullOrBlankSpace.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+
+namespace CommonTestUtilities.ErrorsClassData;
+
+public class EmptyNullOrBlankSpace : IEnumerable<object[]>
+{
+    public IEnumerator<object[]> GetEnumerator()
+    {
+        yield return new object[] { "" };
+#pragma warning disable CS8625
+        yield return new object[] { null };
+#pragma warning restore CS8625
+        yield return new object[] { "     " };
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
