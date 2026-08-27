2512a03843c6c7ca801984ea32dba7be31bcc1db
Mateus <matt_ufo2@hotmail.com>
2026-08-27 10:39:13 -0300

    feat(application): implementa caso de uso LoginWithEmailAndPasswordUseCase
    - Criada a interface ILoginWithEmailAndPasswordUseCase definindo o contrato para a autenticação.
    - Implementada a classe LoginWithEmailAndPasswordUseCase para buscar o usuário por e-mail, validar a senha com o password hasher, tratar exceções de login inválido e retornar a resposta com sucesso.

---
+++ src/Backend/WestmarchBook.Application/UseCases/Login/WithEmailAndPassword/ILoginWithEmailAndPasswordUseCase.cs
@@ -0,0 +1,9 @@
+﻿using WestmarchBook.Communication.Requests;
+using WestmarchBook.Communication.Responses;
+
+namespace WestmarchBook.Application.UseCases.Login.WithEmailAndPassword;
+
+public interface ILoginWithEmailAndPasswordUseCase
+{
+    Task<ResponseRegisterUserJson> Execute(RequestLoginJson request);
+}
---
+++ src/Backend/WestmarchBook.Application/UseCases/Login/WithEmailAndPassword/LoginWithEmailAndPasswordUseCase.cs
@@ -0,0 +1,41 @@
+﻿using WestmarchBook.Communication.Requests;
+using WestmarchBook.Communication.Responses;
+using WestmarchBook.Domain.Repositories.User;
+using WestmarchBook.Domain.Security.PasswordHashing;
+using WestmarchBook.Exception.ExceptionsBase;
+
+namespace WestmarchBook.Application.UseCases.Login.WithEmailAndPassword;
+
+public class LoginWithEmailAndPasswordUseCase : ILoginWithEmailAndPasswordUseCase
+{
+    private readonly IUserReadOnlyRepository _userReadRepository;
+    private readonly IPasswordHasher _passwordHasher;
+
+    public LoginWithEmailAndPasswordUseCase(IUserReadOnlyRepository userReadRepository, IPasswordHasher passwordHasher)
+    {
+        _userReadRepository = userReadRepository;
+        _passwordHasher = passwordHasher;
+    }
+
+    public async Task<ResponseRegisterUserJson> Execute(RequestLoginJson request)
+    {
+        var user = await _userReadRepository.GetByEmail(request.Email);
+
+        if (user == null)
+        {
+            throw new InvalidLoginException();
+        }
+
+        var isPasswordValid = _passwordHasher.VerifyPassword(request.Password, user.Password);
+
+        if (isPasswordValid == false)
+        {
+            throw new InvalidLoginException();
+        }
+
+        return new ResponseRegisterUserJson
+        {
+            Name = user.Name
+        };
+    }
+}
