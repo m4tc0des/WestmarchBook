6fa13a089fca4d9514518f622a4fbb32f93ae84d
Mateus <matt_ufo2@hotmail.com>
2026-08-28 17:42:47 -0300

    test(utilities): adiciona RequestLoginJsonBuilder para suporte aos testes de login
    - Criada a classe RequestLoginJsonBuilder utilizando o Bogus para gerar solicitações de login falsificadas com e-mail e senha aleatórios.

---
+++ tests/CommonTestUtilities/Requests/RequestLoginJsonBuilder.cs
@@ -0,0 +1,14 @@
+﻿using Bogus;
+using WestmarchBook.Communication.Requests;
+
+namespace CommonTestUtilities.Requests;
+
+public class RequestLoginJsonBuilder
+{
+    public static RequestLoginJson Build()
+    {
+        return new Faker<RequestLoginJson>()
+            .RuleFor(request => request.Email, faker => faker.Internet.Email())
+            .RuleFor(request => request.Password, faker => faker.Internet.Password());      
+    }
+}
