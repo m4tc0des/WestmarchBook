7e64eeca738acb6f37c5ad36ddf763ee73d051c6
Mateus <matt_ufo2@hotmail.com>
2026-08-29 09:34:57 -0300

    refactor(tests): atualiza UserBuilder para retornar tupla e ajusta testes de login
    - Modificado o método Build do UserBuilder para retornar uma tupla contendo a entidade User e a senha em texto puro.
    - Ajustado o método privado GenerateRandomPassword para gerar e retornar o par de senha crua e senha hasheada.
    - Organizados os testes unitários de LoginWithEmailAndPasswordUseCase para utilizar o novo formato do UserBuilder.

--- tests/CommonTestUtilities/Entities/UserBuilder.cs
+++ tests/CommonTestUtilities/Entities/UserBuilder.cs
@@ -6,19 +6,22 @@ namespace CommonTestUtilities.Entities;
 
 public class UserBuilder
 {
-    public static User Build()
+    public static (User user, string password) Build()
     {
-        return new Faker<User>()
+        var (password, passwordHashed) = GenerateRandomPassword();
+        var user = new Faker<User>()
         .RuleFor(user => user.Name, faker => faker.Person.FirstName)
-        .RuleFor(user => user.Email, faker => faker.Internet.Email())
-        .RuleFor(user => user.Password, _ => GenerateRandomPassword());
+        .RuleFor(user => user.Email, (faker, user) => faker.Internet.Email(user.Name))
+        .RuleFor(user => user.Password, _ => passwordHashed);
+
+        return (user, password);
     }
 
-    private static string GenerateRandomPassword()
+    private static (string password, string passwordHashed) GenerateRandomPassword()
     {
         var passwordEncripter = new IPasswordHasherBuilder().Build();
         var password = new Faker().Internet.Password();
 
-        return passwordEncripter.PasswordHash(password);
+        return (password, passwordEncripter.HashPassword(password));
     }
 }
---
+++ tests/UseCases.Tests/Login/WithEmailAndPassword/LoginWithEmailAndPasswordUseCaseTests.cs
@@ -0,0 +1,85 @@
+﻿using CommonTestUtilities.Entities;
+using CommonTestUtilities.Repositories;
+using CommonTestUtilities.Requests;
+using CommonTestUtilities.Security;
+using Shouldly;
+using System.Net;
+using WestmarchBook.Application.UseCases.Login.WithEmailAndPassword;
+using WestmarchBook.Exception;
+using WestmarchBook.Exception.ExceptionsBase;
+
+namespace UseCases.Tests.Login.WithEmailAndPassword;
+
+public class LoginWithEmailAndPasswordUseCaseTests
+{
+    [Fact]
+    public async Task Success()
+    {
+        var (user, _) = UserBuilder.Build();
+        var request = RequestLoginJsonBuilder.Build();
+
+        request.Email = user.Email;
+
+        var useCase = CreateUseCase(request.Password, user);
+        var result = await useCase.Execute(request);
+
+        result.ShouldNotBeNull();
+        result.Tokens.ShouldNotBeNull();
+        result.Name.ShouldBe(user.Name);
+        result.Tokens.AccessToken.ShouldBeNullOrEmpty();
+        result.Tokens.RefreshToken.ShouldBeNullOrEmpty();
+    }
+
+    [Fact]
+    public async Task Validate_ShouldThrowException_When_UserDontExist()
+    {
+        var request = RequestLoginJsonBuilder.Build();
+        var useCase = CreateUseCase();
+        var exception = await useCase.Execute(request).ShouldThrowAsync<InvalidLoginException>();
+
+        exception.GetStatusCode().ShouldBe(HttpStatusCode.Unauthorized);
+
+        exception.GetErrorMessage().ShouldSatisfyAllConditions(errors =>
+        {
+            errors.Count.ShouldBe(1);
+            errors.ShouldContain(ResourceMessagesException.VALIDATION_LOGIN_INVALID);
+        });
+    }
+
+    [Fact]
+    public async Task Validate_ShouldThrowException_When_PasswordIsInvalid()
+    {
+        var (user, _) = UserBuilder.Build();
+        var request = RequestLoginJsonBuilder.Build();
+
+        request.Email = user.Email;
+
+        var useCase = CreateUseCase(user:user);
+        var exception = await useCase.Execute(request).ShouldThrowAsync<InvalidLoginException>();
+
+        exception.GetStatusCode().ShouldBe(HttpStatusCode.Unauthorized);
+
+        exception.GetErrorMessage().ShouldSatisfyAllConditions(errors =>
+        {
+            errors.Count.ShouldBe(1);
+            errors.ShouldContain(ResourceMessagesException.VALIDATION_LOGIN_INVALID);
+        });
+    }
+
+    private LoginWithEmailAndPasswordUseCase CreateUseCase(string? password = null, WestmarchBook.Domain.Entities.User? user = null)
+    {
+        var userReadRepository = new IUserReadOnlyRepositoryBuilder();
+        var passwordHasher = new IPasswordHasherBuilder();
+
+        if (user is not null)
+        {
+            userReadRepository.GetByEmail(user);
+        }
+        if (password is not null and not "")
+        {
+            passwordHasher.VerifyPassword(password);
+        }
+
+        return new LoginWithEmailAndPasswordUseCase(userReadRepository.Build(), passwordHasher.Build());
+    }
+}
