4d548e5aaecdce3832c63acd7d174ef6f9e10f04
Mateus <matt_ufo2@hotmail.com>
2026-08-29 09:36:38 -0300

    test(api): atualiza testes de integração de login com suporte a internacionalização e corrige UserIdentityManager
    - Atualizada a classe LoginWithEmailAndPasswordTests para herdar de BaseIntegrationTest e adicionar teste parametrizado com CultureInlineData para validação de erros localizados.
    - Ajustado o UserIdentityManager para retornar a senha em texto puro através da variável correta.

---
+++ tests/WebApi.Tests/Login/WithEmailAndPassword/LoginWithEmailAndPasswordTests.cs
@@ -0,0 +1,69 @@
+﻿using CommonTestUtilities.Requests;
+using Shouldly;
+using System.Globalization;
+using System.Net;
+using System.Net.Http.Json;
+using System.Text.Json;
+using WebApi.Tests.InlineData;
+using WebApi.Tests.Resources;
+using WestmarchBook.Communication.Requests;
+using WestmarchBook.Exception;
+
+namespace WebApi.Tests.Login.WithEmailAndPassword;
+
+public class LoginWithEmailAndPasswordTests: BaseIntegrationTest
+{
+    private const string REQUEST_URI = "/authentication";
+    private readonly UserIdentityManager _user1;
+
+    public LoginWithEmailAndPasswordTests(WestmarchBookApplicationFactory factory): base(factory)
+    {
+        _user1 = factory.User_1;
+    }
+
+    [Fact]
+    public async Task Success()
+    {
+        var request = new RequestLoginJson
+        {
+            Email = _user1.GetEmail(),
+            Password = _user1.GetPassword(),
+        };
+
+        var response = await Post(REQUEST_URI, request);
+
+        response.StatusCode.ShouldBe(HttpStatusCode.OK);
+
+        await using var responseBody = await response.Content.ReadAsStreamAsync();
+
+        var responseData = await JsonDocument.ParseAsync(responseBody);
+
+        responseData.RootElement.GetProperty("name").GetString().ShouldBe(_user1.GetName());
+        responseData.RootElement.GetProperty("tokens").GetProperty("accessToken").GetString().ShouldBeEmpty();
+        responseData.RootElement.GetProperty("tokens").GetProperty("refreshToken").GetString().ShouldBeEmpty();
+    }
+
+    [Theory]
+    [ClassData(typeof(CultureInlineData))]
+    public async Task Validate_ShouldThrowException_When_UserDontExist(string culture)
+    {
+        var request = RequestLoginJsonBuilder.Build();
+
+        var response = await Post(REQUEST_URI, request, culture);
+
+        response.StatusCode.ShouldBe(HttpStatusCode.Unauthorized);
+
+        var expectedErrorMessage = ResourceMessagesException.ResourceManager.GetString("VALIDATION_LOGIN_INVALID", new CultureInfo(culture));
+
+        await using var responseBody = await response.Content.ReadAsStreamAsync();
+
+        var responseData = await JsonDocument.ParseAsync(responseBody);
+        var errors = responseData.RootElement.GetProperty("errors").EnumerateArray();
+
+        errors.ShouldSatisfyAllConditions(errors =>
+        {
+            errors.Count().ShouldBe(1);
+            errors.ShouldContain(error => error.GetString()!.Equals(expectedErrorMessage));
+        });
+    }
+}
---
+++ tests/WebApi.Tests/Resources/UserIdentityManager.cs
@@ -0,0 +1,33 @@
+﻿namespace WebApi.Tests.Resources;
+
+public class UserIdentityManager
+{
+    private readonly WestmarchBook.Domain.Entities.User _user;
+    private readonly string _password;
+
+    public UserIdentityManager(WestmarchBook.Domain.Entities.User user, string password)
+    {
+        _user = user;   
+        _password = password;
+    }
+
+    public long GetId()
+    {
+        return _user.Id;
+    }
+
+    public string GetName()
+    {
+        return _user.Name;
+    }
+
+    public string GetEmail()
+    {
+        return _user.Email;
+    }
+
+    public string GetPassword()
+    {
+        return _password;
+    }
+}
