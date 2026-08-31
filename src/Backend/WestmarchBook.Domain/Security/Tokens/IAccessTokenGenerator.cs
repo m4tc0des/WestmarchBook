860643a46782e71795093c30b3629de38ff8b9d7
Mateus <matt_ufo2@hotmail.com>
2026-08-31 09:07:05 -0300

    feat(security): adiciona interface IAccessTokenGenerator e implementação do JwtTokenHandler
    - Criada a interface IAccessTokenGenerator na camada de domínio para definir o contrato de geração de tokens de acesso.
    - Implementada a classe JwtTokenHandler na infraestrutura utilizando o JsonWebTokenHandler para estruturar o payload, claims e assinatura criptográfica do JWT.

---
+++ src/Backend/WestmarchBook.Domain/Security/Tokens/IAccessTokenGenerator.cs
@@ -0,0 +1,8 @@
+﻿using WestmarchBook.Domain.Entities;
+
+namespace WestmarchBook.Domain.Security.Tokens;
+
+public interface IAccessTokenGenerator
+{
+    string Generate(User user);
+}
---
+++ src/Backend/WestmarchBook.Infrastructure/Security/Tokens/Access/JwtTokenHandler.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using System.Text;
+using WestmarchBook.Domain.Entities;
+using WestmarchBook.Domain.Security.Tokens;
+
+namespace WestmarchBook.Infrastructure.Security.Tokens.Access;
+
+internal sealed class JwtTokenHandler : IAccessTokenGenerator
+{
+    private readonly uint _expirationTimeMinutes;
+    private readonly string _signingKey;
+
+    public JwtTokenHandler(uint expirationTimeMinutes, string signingKey)
+    {
+        _expirationTimeMinutes = expirationTimeMinutes;
+        _signingKey = signingKey;
+    }
+
+    public string Generate(User user)
+    {
+        var claims = new List<Claim>
+        {
+            new Claim(JwtRegisteredClaimNames.Sub, user.Id.ToString())
+        };
+
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Expires = DateTime.UtcNow.AddMinutes(_expirationTimeMinutes),
+            SigningCredentials = new SigningCredentials(Credentials(), SecurityAlgorithms.HmacSha256),
+            Subject = new ClaimsIdentity(claims)
+        };
+
+        var handler = new JsonWebTokenHandler();
+
+        return handler.CreateToken(tokenDescriptor);
+    }
+
+    private SymmetricSecurityKey Credentials()
+    {
+        var keyBytes = Encoding.UTF8.GetBytes(_signingKey);
+
+        return new SymmetricSecurityKey(keyBytes);
+    }
+}
