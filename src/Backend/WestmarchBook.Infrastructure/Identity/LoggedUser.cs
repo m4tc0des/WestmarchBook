059f2d8aa79bef7f08e03ef0ba121311d7fbc912
Mateus <matt_ufo2@hotmail.com>
2026-09-02 17:47:14 -0300

    feat(infrastructure): implementa a classe LoggedUser para recuperação do usuário autenticado
    - Criada a classe LoggedUser implementando a interface ILoggedUser para gerenciar o contexto de autenticação.
    - Implementado o método GetUserId para extrair e converter o ID do usuário (claim sub) a partir do token JWT utilizando o JsonWebTokenHandler.
    - Implementado o método GetProfile para consultar no banco de dados o registro do usuário ativo correspondente ao ID extraído.

---
+++ src/Backend/WestmarchBook.Infrastructure/Identity/LoggedUser.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.JsonWebTokens;
+using WestmarchBook.Domain.Entities;
+using WestmarchBook.Domain.Identity;
+using WestmarchBook.Domain.Security.Tokens;
+using WestmarchBook.Infrastructure.DataAccess;
+
+namespace WestmarchBook.Infrastructure.Identity;
+
+internal sealed class LoggedUser : ILoggedUser
+{
+    private readonly IAccessTokenProvider _accessTokenProvider;
+    private readonly WestmarchBookDbContext _dbContext;
+    public LoggedUser(IAccessTokenProvider accessTokenProvider, WestmarchBookDbContext dbContext)
+    {
+        _accessTokenProvider = accessTokenProvider;
+        _dbContext = dbContext;
+    }
+
+    public async Task<User> GetProfile()
+    {
+       var userId = GetUserId();
+
+        return await _dbContext.Users.FirstAsync(user => user.Active && user.Id == userId);
+    }
+
+    public long GetUserId()
+    {
+        var accessToken = _accessTokenProvider.GetToken();
+        var handler = new JsonWebTokenHandler();
+        var jsonWebToken = handler.ReadJsonWebToken(accessToken);
+        var subject = jsonWebToken.Claims.First(claim => claim.Type.Equals(JwtRegisteredClaimNames.Sub));
+
+        return long.Parse(subject.Value);
+    }
+}
