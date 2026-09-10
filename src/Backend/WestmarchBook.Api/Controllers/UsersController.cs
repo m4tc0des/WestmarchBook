893d7ce97c1012174f12da3eaa2a0e7a570fb7ab
Mateus <matt_ufo2@hotmail.com>
2026-09-10 17:09:23 -0300

    feat(application): adiciona interface IGetUserProfileUseCase feat(api): implementa endpoint GetUserProfile no UsersController

--- src/Backend/WestmarchBook.Api/Controllers/UsersController.cs
+++ src/Backend/WestmarchBook.Api/Controllers/UsersController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using WestmarchBook.Application.UseCases.User.Profile;
 using WestmarchBook.Application.UseCases.User.Register;
 using WestmarchBook.Communication.Requests;
 using WestmarchBook.Communication.Responses;
@@ -22,8 +23,11 @@ public class UsersController : ControllerBase
 
     [HttpGet]
     [Authorize]
-    public async Task<IActionResult> GetUserProfile()
+    [ProducesResponseType(typeof(ResponseUserProfileJson), StatusCodes.Status200OK)]
+    public async Task<IActionResult> GetUserProfile([FromServices] IGetUserProfileUseCase useCase)
     {
-        return Ok();
+        var result = await useCase.Execute();
+
+        return Ok(result);
     }
 }
---
+++ src/Backend/WestmarchBook.Application/UseCases/User/Profile/IGetUserProfileUseCase.cs
@@ -0,0 +1,8 @@
+﻿using WestmarchBook.Communication.Responses;
+
+namespace WestmarchBook.Application.UseCases.User.Profile;
+
+public interface IGetUserProfileUseCase
+{
+    Task<ResponseUserProfileJson> Execute();
+}
