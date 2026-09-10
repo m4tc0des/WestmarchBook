054363ff5ce9ed8a114570659d732def9cd256d0
Mateus <matt_ufo2@hotmail.com>
2026-09-10 17:10:43 -0300

    feat(application): implementa GetUserProfileUseCase para retorno do perfil do usuário logado feat(ioc): registra IGetUserProfileUseCase no container de injecao de dependencia

--- src/Backend/WestmarchBook.Application/DependencyInjectionExtension.cs
+++ src/Backend/WestmarchBook.Application/DependencyInjectionExtension.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using WestmarchBook.Application.UseCases.Login.WithEmailAndPassword;
+using WestmarchBook.Application.UseCases.User.Profile;
 using WestmarchBook.Application.UseCases.User.Register;
 
 namespace WestmarchBook.Application;
@@ -16,6 +17,7 @@ public static class DependencyInjectionExtension
         {
             services.AddScoped<IRegisterUserAccountUseCase, RegisterUserAccountUseCase>();
             services.AddScoped<ILoginWithEmailAndPasswordUseCase, LoginWithEmailAndPasswordUseCase>();
+            services.AddScoped<IGetUserProfileUseCase, GetUserProfileUseCase>();
         }
     }
 }
---
+++ src/Backend/WestmarchBook.Application/UseCases/User/Profile/GetUserProfileUseCase.cs
@@ -0,0 +1,21 @@
+﻿using Mapster;
+using WestmarchBook.Communication.Responses;
+using WestmarchBook.Domain.Identity;
+
+namespace WestmarchBook.Application.UseCases.User.Profile;
+
+public class GetUserProfileUseCase : IGetUserProfileUseCase
+{
+    private readonly ILoggedUser _loggedUser;
+    public GetUserProfileUseCase(ILoggedUser loggedUser)
+    {
+        _loggedUser = loggedUser;
+    }
+
+    public async Task<ResponseUserProfileJson> Execute()
+    {
+        var loggedUser = await _loggedUser.GetProfile();
+
+        return loggedUser.Adapt<ResponseUserProfileJson>();
+    }
+}
