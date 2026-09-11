b2916c63a6260be852bcf3e60386625a208582be
Mateus <matt_ufo2@hotmail.com>
2026-09-11 17:37:27 -0300

    test(tests): adicionar builder para ILoggedUser e testes unitários para o GetUserProfileUseCase

---
+++ tests/CommonTestUtilities/Identity/ILoggedUserBuilder.cs
@@ -0,0 +1,18 @@
+﻿using Moq;
+using WestmarchBook.Domain.Entities;
+using WestmarchBook.Domain.Identity;
+
+namespace CommonTestUtilities.Identity;
+
+public class ILoggedUserBuilder
+{
+    public static ILoggedUser Build(User user)
+    {
+        var mock = new Mock<ILoggedUser>();
+
+        mock.Setup(x => x.GetProfile()).ReturnsAsync(user);
+        mock.Setup(x => x.GetUserId()).Returns(user.Id);
+
+        return mock.Object;
+    }
+}
---
+++ tests/UseCases.Tests/Profile/GetUserProfileUseCaseTests.cs
@@ -0,0 +1,28 @@
+﻿using CommonTestUtilities.Entities;
+using CommonTestUtilities.Identity;
+using Shouldly;
+using WestmarchBook.Application.UseCases.User.Profile;
+
+namespace UseCases.Tests.Profile;
+
+public class GetUserProfileUseCaseTests
+{
+    [Fact]
+    public async Task Success()
+    {
+        (var user, _) = UserBuilder.Build();
+        var useCase = CreateUseCase(user);
+        var result = await useCase.Execute();
+
+        result.ShouldNotBeNull();
+        result.Name.ShouldBe(user.Name);
+        result.Email.ShouldBe(user.Email);
+    }
+
+    private GetUserProfileUseCase CreateUseCase(WestmarchBook.Domain.Entities.User user)
+    {
+        var loggedUser = ILoggedUserBuilder.Build(user);
+
+        return new GetUserProfileUseCase(loggedUser);
+    }
+}
